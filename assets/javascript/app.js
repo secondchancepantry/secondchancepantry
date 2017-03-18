421b5a29dd64d8df2c822cdbcb328c245cc1d9bd
Michelle Dai <michelledai2020@gmail.com>
2017-03-18 12:44:00 -0700

    Firebase setup working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,27 @@
 var ingredients;
+var pantryList = [];
+var selectedIngredientList = [];
+var database = firebase.database();
+var userRef = firebase.database().ref('/users');
 
 $('#submit-btn').on('click', function() {
     ingredients = $('#ingredient-input').val().trim();
-    console.log(ingredients);
+     selectedIngredientList.push(ingredients);
      $('#ingredient-container').prepend(`<p><button class='btn btn-success ingredient'>${ingredients}</button></p>`);
      $('#ingredient-input').val('');
+
+    console.log(selectedIngredientList);
+    userRef.set({
+        id: "user",
+        ingredients:selectedIngredientList
+    });
+
 })
+
+
 
 $(document).on('click', '.ingredient', function() {
     var selectedIngredient = $(this).text();
-    // console.log(selectedIngredient);
     $('#selected-ingredients-container').append(`<p class='selectedIngredient'>${selectedIngredient}</p>`);
 
 });
