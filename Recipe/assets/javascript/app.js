85fde9fc9010589c8492cbc9ae5fe61616dfe442
Michelle Dai <michelledai2020@gmail.com>
2017-03-21 16:15:19 -0700

    Beautiful

---
+++ Recipe/assets/javascript/app.js
@@ -0,0 +1,156 @@
+var ingredients;
+var pantryList = [];
+var IngredientList = [];
+var selectedIngredientList = [];
+var database = firebase.database();
+var userRef = firebase.database().ref('/users');
+
+
+/*===============================
+=            TESTING            =
+===============================*/
+$(function() {
+  // SETUP
+  var $list, $newItemForm, $newItemButton;
+  var item = '';
+  $list = $('ul');
+  $newItemForm = $('#newItemForm');
+  $newItemButton = $('#newItemButton');
+
+  $('li').hide().each(function(index) {
+    $(this).delay(450 * index).fadeIn(1600);
+  });
+  // ITEM COUNTER
+  function updateCount() {
+    var items = $('li[class!=complete]').length;
+    $('#counter').text(items);
+  }
+  updateCount();
+
+  // SETUP FORM FOR NEW ITEMS
+  $newItemButton.show();
+  $newItemForm.hide();
+  $('#showForm').on('click', function() {
+    $newItemButton.hide();
+    $newItemForm.show();
+  });
+
+  // ADDING A NEW LIST ITEM
+  $newItemForm.on('submit', function(e) {
+    e.preventDefault();
+    var text = $('input:text').val();
+    $list.append('<li>' + text + '</li>');
+    $('input:text').val('');
+    updateCount();
+  });
+
+  //CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
+  $list.on('click', 'li', function() {
+    var $this = $(this);
+    var complete = $this.hasClass('complete');
+    var selectedIngredient = $(this).text();
+    $('#selected-ingredients-container').append(`<p class='selectedIngredient'>${selectedIngredient}</p>`);
+    selectedIngredientList.push(selectedIngredient);
+    console.log(selectedIngredientList);
+
+    if (complete === true) {
+      $this.animate({
+        opacity: 0.0,
+        paddingLeft: '+=180'
+      }, 500, 'swing', function() {
+        $this.remove();
+      });
+    } else {
+      item = $this.text();
+      $this.remove();
+      $list
+      // .append('<li class=\"complete\">' + item + '</li>')
+      .hide().fadeIn(300);
+      updateCount();
+    }
+  });
+
+});
+
+
+
+
+/*=====  End of TESTING  ======*/
+
+
+
+
+
+
+
+
+
+
+
+// $('#submit-btn').on('click', function() {
+//     ingredients = $('#ingredient-input').val().trim();
+//      IngredientList.push(ingredients);
+//      $('#ingredient-container').prepend(`<p><button class='btn btn-success ingredient'>${ingredients}</button></p>`);
+//      $('#ingredient-input').val('');
+
+//     console.log(IngredientList);
+//     userRef.set({
+//         id: "user",
+//         ingredients:IngredientList
+//     });
+
+// })
+
+// $(document).on('click', '.ingredient', function() {
+//     var selectedIngredient = $(this).text();
+//     $('#selected-ingredients-container').append(`<p class='selectedIngredient'>${selectedIngredient}</p>`);
+//     selectedIngredientList.push(selectedIngredient);
+//     console.log(selectedIngredientList);
+// });
+
+var getRecipe = function(){
+    return $.ajax({
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=' + selectedIngredientList.join(",") + '&limitLicense=false&number=5&ranking=1', // The URL to the API. You can get this in the API page of the API you intend to consume
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {}, // Additional parameters here
+        dataType: 'json',
+        success: function(data) { 
+
+            var results = data;
+            console.log(results);
+
+            for (var i = 0; i < results.length; i++) {
+            var recipe = results[i].id;
+            console.log(recipe);
+
+                $.ajax({
+                    url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + recipe + '/information?includeNutrition=false', // The URL to the API. You can get this in the API page of the API you intend to consume
+                    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+                    data: {}, // Additional parameters here
+                    dataType: 'json',
+                    success: function(data) { console.log((data)); },
+                    error: function(err) { alert(err); },
+                    beforeSend: function(xhr) {
+                    xhr.setRequestHeader("X-Mashape-Authorization", "DrNwVlaI6BmshQBWAGcWVKEwd2Nop1lT1sHjsnhKbHXzD5wrkP"); // Enter here your Mashape key
+                    }
+                });
+
+            };
+
+         },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+        xhr.setRequestHeader("X-Mashape-Authorization", "DrNwVlaI6BmshQBWAGcWVKEwd2Nop1lT1sHjsnhKbHXzD5wrkP"); // Enter here your Mashape key
+        }
+    });
+};
+
+
+$('#search-btn').on('click', function(){
+
+    getRecipe();
+
+});
+
+;
+
