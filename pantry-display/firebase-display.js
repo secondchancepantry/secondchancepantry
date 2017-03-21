b396a04d3203a5ef70a24c7c678a1183723e66e7
Guillermo Lopez <thatgee@calvisitor-10-105-145-93.calvisitor.1918.berkeley.edu>
2017-03-20 21:57:10 -0700

    Update js files.

--- pantry-display/firebase-display.js
+++ pantry-display/firebase-display.js
@@ -11,21 +11,58 @@
 
   var database = firebase.database();
 
-  database.ref.on("value", function(snapshot) {
-  	
-  console.log(snapshot.val());
-  $("#ingredients-item1").val().trim().text(snapshot);
-  $("#ingredients-item2").val().trim().text(snapshot);
-  $("#ingredients-item3").val().trim().text(snapshot);
+  database.ref().on("value", function(snapshot) {
+    
+  
+  var data = snapshot.val();
+  var ingredients = data.users.ingredients;
 
+
+  ingredientsInDatabase(ingredients);
+  expire(ingredients);
+  
 
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
 
-$("#ingredients-item1").on('click'.function(){
+function expire(snapshot) {
 
-   $('#ingredients-item1').hide()
+    $( "#date-picker" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+   
+};
 
+
+function ingredientsInDatabase(snapshot) {
+
+     var expire = $( "#date-picker" ).datepicker({
+      changeMonth: true,
+      changeYear: true
+      });
+  
+     
+      var insideList = snapshot;
+      
+     
+      if (!Array.isArray(insideList)) {
+        insideList = [];
+      }
+   
+      for (var i = 0; i < insideList.length; i++) {
+        var ingsButton = $("<button class='ingredient-button delete'>").text(insideList[i]).attr("data-index", i);
+        var expireButton = $( "<p>Date: <input type='text' id='date-picker'></p>");
+        $("#ingredients-list").prepend(ingsButton);
+        $("#expire-button").prepend(expireButton);
+      }
+};
+
+$(document).on("click", "button.delete", function(event) {
+
+      event.preventDefault();
+
+      $(this).remove();
 });
-
+    
