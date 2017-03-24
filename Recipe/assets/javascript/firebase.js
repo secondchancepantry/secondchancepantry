6fcd7cf80783db8504f1b656c22afd1ecc527168
jakestenger <myselfjake@gmail.com>
2017-03-23 19:57:00 -0700

    cleaned up, put index.html in place

---
+++ Recipe/assets/javascript/firebase.js
@@ -0,0 +1,8 @@
+var fb_config = {
+	apiKey: "AIzaSyAjLOCnFetZBaMBqLoAxQOJiuve81ijJlU",
+	authDomain: "secondchancepantry-f65c5.firebaseapp.com",
+	databaseURL: "https://secondchancepantry-f65c5.firebaseio.com",
+	storageBucket: "secondchancepantry-f65c5.appspot.com",
+	messagingSenderId: "1083855170391"
+};
+firebase.initializeApp(fb_config);
